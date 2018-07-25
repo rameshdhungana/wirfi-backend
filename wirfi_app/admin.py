6ef909bdb7d01d8d42989d3974b62eee975536b0
Sumedh <ssumedhiw@gmail.com>
2018-07-25 13:20:16 +0545

    [Customized] UserModel using AbstractBaseUser

--- wirfi_app/admin.py
+++ wirfi_app/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile, Business, Billing, Device
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User)
 admin.site.register([Profile, Billing, Business, Device])
--- wirfi_web/settings.py
+++ wirfi_web/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'rest_auth',
     'rest_auth.registration',
     'allauth',
+    'allauth.account',
 
     'wirfi_app',
 ]
@@ -129,6 +130,13 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Account Settings
+ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = None
+ACCOUNT_USERNAME_REQUIRED = False
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
