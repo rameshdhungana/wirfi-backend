1d7b5e2ab65820e2db7d731ce19054fa6e122d9e
Ramesh Dhungana <drameshiw@gmail.com>
2018-08-20 14:45:05 +0545

    [fixed] merge conflicts fixed

--- wirfi_app/views.py
+++ wirfi_app/views.py
@@ -362,7 +362,6 @@ class BillingView(generics.ListCreateAPIView):
 
         headers = self.get_success_headers(serializer.data)
 
-        
         # print(data)
         return Response(data, status=status.HTTP_200_OK, headers=headers)
 
