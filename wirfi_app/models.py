30700dba827b308522e77359154f967b45439ce4
Ramesh Dhungana <drameshiw@gmail.com>
2018-10-29 17:55:55 +0545

    [change] is_asleep value in device setting is changed to false as default

--- wirfi_app/models.py
+++ wirfi_app/models.py
@@ -193,7 +193,7 @@ class DeviceSetting(models.Model):
     mute_duration = models.IntegerField(null=True, blank=True)
     priority = models.BooleanField(default=False)
     has_sleep_feature = models.BooleanField(default=False)
-    is_asleep = models.BooleanField(default=True)
+    is_asleep = models.BooleanField(default=False)
     sleep_start = models.DateTimeField(auto_now=True)
     sleep_duration = models.IntegerField(null=True, blank=True)
 
