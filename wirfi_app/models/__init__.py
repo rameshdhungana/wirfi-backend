3c1a23c8a5d548cb116e946ed1d4cf343dafecda
Ramesh Dhungana <drameshiw@gmail.com>
2018-11-26 16:55:08 +0545

    [modified] modified the devicequeuetask model to include action and device fields which are unique together

--- wirfi_app/models/__init__.py
+++ wirfi_app/models/__init__.py
@@ -1,11 +1,11 @@
 from wirfi_app.models.authorization_token import AuthorizationToken, UserActivationCode
+from wirfi_app.models.device import Device
+from wirfi_app.models.device_queued_tasks import QueueTaskForWiRFiDevice
 from wirfi_app.models.device_information import DeviceLocationHours, DeviceNetwork, DeviceStatus, DEVICE_STATUS
 from wirfi_app.models.device_services import DeviceCameraServices, DeviceNotification, NOTIFICATION_TYPE, READ
 from wirfi_app.models.device_settings import DeviceSetting
 from wirfi_app.models.device_subscription_plan import ServicePlan, Subscription
-from wirfi_app.models.device import Device
 from wirfi_app.models.industry_franchise_type import Industry, Franchise
 from wirfi_app.models.preset_filter import PresetFilter
 from wirfi_app.models.user_detail import Profile, Billing, Business
 from wirfi_app.models.user import User
-from wirfi_app.models.device_queued_tasks import QueueTaskForWiRFiDevice
--- wirfi_app/models/device_queued_tasks.py
+++ wirfi_app/models/device_queued_tasks.py
@@ -1,8 +1,28 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 
+from wirfi_app.models import Device
+
+PRIMARY_NETWORK_CHANGED = 'primary_network_changed'
+SECONDARY_NETWORK_CHANGED = 'secondary_network_changed'
+DEVICE_CREATED = 'device_created'
+NO_TASK = 'no task'
+
+ACTIONS = (
+    (PRIMARY_NETWORK_CHANGED, 'primary_network_changed'),
+    (SECONDARY_NETWORK_CHANGED, 'secondary_network_changed'),
+    (DEVICE_CREATED, 'device_created'),
+    (NO_TASK, 'no task')
+)
+
 
 class QueueTaskForWiRFiDevice(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     data = JSONField()
     queued_status = models.BooleanField(default=True)
+    action = models.CharField(choices=ACTIONS, default=NO_TASK, max_length=64)
+    device = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='queue_tasks')
+
+    class Meta:
+        ordering = ('created_at',)
+        unique_together = ('action', 'device')
