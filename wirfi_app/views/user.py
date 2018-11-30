3becdd0a12df67a04ec79a161ecd6fcfbe68e5c2
Sumedh <ssumedhiw@gmail.com>
2018-11-30 16:36:52 +0545

    User's list api response changed.

--- wirfi_app/views/user.py
+++ wirfi_app/views/user.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
+from allauth.account import app_settings as allauth_settings
+from allauth.account.utils import complete_signup
+
 from rest_framework import generics, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -71,5 +74,44 @@ def profile_images_view(request, id):
 
 class UserListCreateView(generics.ListCreateAPIView):
     serializer_class = AdminUserSerializer
-    queryset = User.objects.all()
     pagination_class = ActivityLogPagination
+
+    def get_queryset(self):
+        return User.objects.exclude(pk=self.request.user.id)
+
+    def list(self, request, *args, **kwargs):
+        if not request.user.is_superuser:
+            return Response({
+                'code': getattr(settings, 'SUCCESS_CODE', 1),
+                'message': 'You do not have permission.',
+            }, status=status.HTTP_403_FORBIDDEN)
+
+        response = super().list(request, *args, **kwargs)
+        response.data = {
+            'code': getattr(settings, 'SUCCESS_CODE', 1),
+            'message': 'Successfully fetched users list data.',
+            'data': response.data
+        }
+        return response
+
+    # def perform_create(self, serializer):
+    #     user = serializer.save(self.request)
+    #     complete_signup(self.request._request, user,
+    #                     allauth_settings.EMAIL_VERIFICATION,
+    #                     None)
+    #     return user
+
+    # def create(self, request, *args, **kwargs):
+    #     if not request.user.is_superuser:
+    #         return Response({
+    #             'code': getattr(settings, 'SUCCESS_CODE', 1),
+    #             'message': 'You do not have permission.',
+    #         }, status=status.HTTP_403_FORBIDDEN)
+    #
+    #     response = super().create(request, *args, **kwargs)
+    #     response.data = {
+    #         'code': getattr(settings, 'SUCCESS_CODE', 1),
+    #         'message': 'Successfully created users list data.',
+    #         'data': response.data
+    #     }
+    #     return response
