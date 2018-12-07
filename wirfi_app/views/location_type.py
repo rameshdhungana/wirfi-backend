52c4f4d7a729fb4e30197dc047305daa128288a1
Sumedh <ssumedhiw@gmail.com>
2018-12-07 16:15:52 +0545

    record on location type create, update and delete

--- wirfi_app/views/location_type.py
+++ wirfi_app/views/location_type.py
@@ -5,13 +5,13 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 
 from wirfi_app.models import Franchise
-from wirfi_app.views.login_logout import get_token_obj
+from wirfi_app.views.create_admin_activity_log import create_activity_log
 from wirfi_app.serializers import LocationTypeSerializer
 
 
-def franchise_type_name_already_exits(name, token):
+def franchise_type_name_already_exits(name, user):
     for key, value in enumerate(
-            Franchise.objects.filter(Q(user__isnull=True) | Q(user=token.user)).values_list('name', flat=True)):
+            Franchise.objects.filter(Q(user__isnull=True) | Q(user=user)).values_list('name', flat=True)):
         if name.upper() == value.upper():
             return True
         return False
@@ -21,8 +21,7 @@ class LocationTypeListView(generics.ListCreateAPIView):
     serializer_class = LocationTypeSerializer
 
     def get_queryset(self):
-        token = get_token_obj(self.request.auth)
-        return Franchise.objects.filter(user=token.user).order_by('id')
+        return Franchise.objects.filter(user=self.request.auth.user).order_by('id')
 
     def list(self, request, *args, **kwargs):
         location_types = self.get_queryset()
@@ -35,20 +34,27 @@ class LocationTypeListView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        token = get_token_obj(self.request.auth)
+        user = request.auth.user
 
         # case sensitive validation of name
-        if franchise_type_name_already_exits(request.data['name'], token):
+        if franchise_type_name_already_exits(request.data['name'], user):
             return Response({
                 'code': getattr(settings, 'ERROR_CODE', 0),
                 'message': "Franchise type with this name already exists.",
 
             }, status=status.HTTP_400_BAD_REQUEST)
 
-        data['user'] = token.user.id
+        data['user'] = user.id
         serializer = LocationTypeSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+
+        create_activity_log(
+            request,
+            "Location Type '{name}' added to user '{email}'.".format(name=serializer.data['name'],
+                                                                     email=user.email)
+        )
+
         return Response({
             'code': getattr(settings, 'SUCCESS_CODE', 1),
             'message': "Successfully added location type.",
@@ -61,26 +67,31 @@ class LocationTypeDetailView(generics.UpdateAPIView, generics.DestroyAPIView):
     serializer_class = LocationTypeSerializer
 
     def get_queryset(self):
-        token = get_token_obj(self.request.auth)
-        return Franchise.objects.filter(user=token.user)
+        return Franchise.objects.filter(user=self.request.auth.user)
 
     def update(self, request, *args, **kwargs):
         franchise_type = self.get_object()
-        token = get_token_obj(self.request.auth)
+        user = request.auth.user
 
         # case sensitive validation of name
-        if franchise_type_name_already_exits(request.data['name'], token):
+        if franchise_type_name_already_exits(request.data['name'], user):
             return Response({
                 'code': getattr(settings, 'ERROR_CODE', 0),
                 'message': "Franchise type with this name already exists.",
 
             }, status=status.HTTP_400_BAD_REQUEST)
 
-        data = {**request.data, 'user': token.user.id}
+        data = {**request.data, 'user': user.id}
         serializer = LocationTypeSerializer(franchise_type, data=data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        serializer.save(user=token.user)
+        create_activity_log(
+            request,
+            "Location Type '{name}' of user '{email}' updated.".format(name=serializer.data['name'],
+                                                                       email=user.email)
+        )
+
         return Response({
             'code': getattr(settings, 'SUCCESS_CODE', 1),
             'message': "Successfully updated franchise type.",
@@ -89,6 +100,12 @@ class LocationTypeDetailView(generics.UpdateAPIView, generics.DestroyAPIView):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        user = request.auth.user
+        create_activity_log(
+            request,
+            "Location Type '{name}' of user '{email}' deleted.".format(name=instance.name,
+                                                                       email=user.email)
+        )
         self.perform_destroy(instance)
         return Response({
             'code': getattr(settings, 'SUCCESS_CODE', 1),
