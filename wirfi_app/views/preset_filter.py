dd45ba1f98660b983e9396ed0ce98660f834cf12
Sumedh <ssumedhiw@gmail.com>
2018-12-07 16:15:19 +0545

    record on preset_filter's create and delete

--- wirfi_app/views/preset_filter.py
+++ wirfi_app/views/preset_filter.py
@@ -5,15 +5,14 @@ from rest_framework.response import Response
 
 from wirfi_app.models import PresetFilter
 from wirfi_app.serializers import PresetFilterSerializer
-from wirfi_app.views.login_logout import get_token_obj
+from wirfi_app.views.create_admin_activity_log import create_activity_log
 
 
 class PresetFilterView(generics.ListCreateAPIView):
     serializer_class = PresetFilterSerializer
 
     def get_queryset(self):
-        token = get_token_obj(self.request.auth)
-        return PresetFilter.objects.filter(user=token.user)
+        return PresetFilter.objects.filter(user=self.request.auth.user)
 
     def list(self, request, *args, **kwargs):
         presets = self.get_queryset()
@@ -26,11 +25,14 @@ class PresetFilterView(generics.ListCreateAPIView):
 
     def create(self, request, *args, **kwargs):
         data = request.data
-        token = get_token_obj(self.request.auth)
+        user = request.auth.user
         serializer = PresetFilterSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         try:
-            serializer.save(user=token.user)
+            serializer.save(user=user)
+            create_activity_log(request,
+                                "Preset '{preset}' added to user '{email}'.".format(preset=serializer.data['name'],
+                                                                                    email=user.email))
             return Response({
                 "code": getattr(settings, "SUCCESS_CODE", 1),
                 "message": "Successfully fetched preset filter data.",
@@ -39,7 +41,7 @@ class PresetFilterView(generics.ListCreateAPIView):
         except:
             return Response({
                 "code": getattr(settings, "ERROR_CODE", 0),
-                "messsage": "This preset name already exists",
+                "message": "This preset name already exists",
             }, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -48,11 +50,13 @@ class PresetFilterDeleteView(generics.RetrieveAPIView, generics.DestroyAPIView):
     lookup_field = 'id'
 
     def get_queryset(self):
-        token = get_token_obj(self.request.auth)
-        return PresetFilter.objects.filter(user=token.user).filter(pk=self.kwargs['id'])
+        return PresetFilter.objects.filter(user=self.request.auth.user).filter(pk=self.kwargs['id'])
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
+        user = request.auth.user
+        create_activity_log(request, "Preset '{preset}' of user '{email}' deleted.".format(preset=instance.name,
+                                                                                           email=user.email))
         self.perform_destroy(instance)
         return Response({
             'code': getattr(settings, 'SUCCESS_CODE', 1),
@@ -61,10 +65,9 @@ class PresetFilterDeleteView(generics.RetrieveAPIView, generics.DestroyAPIView):
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
-        serializer = PresetFilterSerializer(instance)
 
         return Response({
             'code': getattr(settings, 'SUCCESS_CODE', 1),
             'message': "Preset Detail is fetched successfully.",
-            "data": serializer.data
+            "data": PresetFilterSerializer(instance).data
         }, status=status.HTTP_200_OK)
