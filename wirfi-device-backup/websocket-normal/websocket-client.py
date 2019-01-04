2b3298c7f314fd259165c11b9c981167fbdc2779
Ramesh Dhungana <drameshiw@gmail.com>
2019-01-04 15:03:07 +0545

    [update] creating  different  socket scripts for test

--- wirfi-device-backup/websocket-normal/websocket-client.py
+++ wirfi-device-backup/websocket-normal/websocket-client.py
@@ -17,19 +17,19 @@ async def hello():
         greeting = await websocket.recv()
         print(f"< {greeting}")
 
-        import cv2
-        import numpy as np
-        import socket
-        import sys
-        import pickle
-        import struct  ### new code
-        cap = cv2.VideoCapture(0)
-        clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientsocket.connect(('localhost', 8081))
-        while True:
-            ret, frame = cap.read()
-            data = pickle.dumps(frame)  ### new code
-            clientsocket.sendall(struct.pack("H", len(data)) + data)
+        # import cv2
+        # import numpy as np
+        # import socket
+        # import sys
+        # import pickle
+        # import struct  ### new code
+        # cap = cv2.VideoCapture(0)
+        # clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # clientsocket.connect(('localhost', 8081))
+        # while True:
+        #     ret, frame = cap.read()
+        #     data = pickle.dumps(frame)  ### new code
+        #     clientsocket.sendall(struct.pack("H", len(data)) + data)
 
 
 asyncio.get_event_loop().run_until_complete(hello())
